bcd995630e8c5f094bee620a16e527ab3e8b110b
Brahmia-Malek <131770411+Brahmia-Malek@users.noreply.github.com>
2023-08-07 15:34:31 +0200

    Add files via upload

---
+++ Day5/Core/Daikichi_Path_Variables/src/test/java/com/malek/DaikichiPathVaribales/DaikichiPathVariablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malek.DaikichiPathVaribales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiPathVariablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
